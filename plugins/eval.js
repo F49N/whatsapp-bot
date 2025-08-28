5b56afb7d700798f8d7f4d7ad8e5aca6a49c3c52
naxor <naxordeve@gmail.com>
2025-08-28 21:56:46 +0200

    naxor

--- plugins/eval.js
+++ plugins/eval.js
@@ -6,13 +6,14 @@ Module({
 })(async (message) => {
   if (!message.body.startsWith('>')) return;
   let sender = message.sender.split('@')[0];
-  let sudo = process.env.SUDO ? process.env.SUDO.split(',') : [];
+  let sudo = process.env.SUDO ? process.env.SUDO.split(',').map(u => u.trim().replace(/\+/, '')) : [];
   if (!(message.fromMe || sudo.includes(sender))) return;
   let code = message.body.slice(1).trim();
-  try { let result = await eval(`(async () => { ${code} })()`);
-  if (typeof result !== 'string') result = util.inspect(result);
-  await message.send(result);
+  try {
+    let result = await eval(`(async () => { ${code} })()`);
+    if (typeof result !== 'string') result = util.inspect(result);
+    await message.send(result);
   } catch (e) {
-  await message.send(e.message);
+    await message.send(e.message);
   }
 });
