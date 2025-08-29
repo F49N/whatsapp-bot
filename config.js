aee7fff33cb0065f36d255234c22212be36ab2c0
naxor <naxordeve@gmail.com>
2025-08-29 11:30:20 +0200

    naxor

--- config.js
+++ config.js
@@ -12,10 +12,6 @@ module.exports = {
     maxFileSize: parseInt(process.env.MAX_FILE_SIZE) || 100 * 1024 * 1024,
     timezone: process.env.TIMEZONE || 'UTC',
     WORK_TYPE: process.env.WORK_TYPE || 'public',
-    STATUS_REACT: isTrue(process.env.STATUS_REACT) || false, // true 
-    WARN_MESSAGE: process.env.WARN_MESSAGE || '⚠️WARNING⚠️\n*User :* &mention\n*Warn :* &warn\n*Remaining :* &remaining',
-    WARN_RESET_MESSAGE: process.env.WARN_RESET_MESSAGE ||
-        'WARN RESET\nUser : &mention\nRemaining : &remaining',
-    WARN_KICK_MESSAGE: process.env.WARN_KICK_MESSAGE ||
-        '&mention kicked',
+    STATUS_REACT: isTrue(process.env.STATUS_REACT) || false // true 
+    
 };
