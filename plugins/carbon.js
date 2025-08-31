22b6a3f54ef2a97d09efdc841072f198bf878355
naxor <naxordeve@gmail.com>
2025-08-31 17:45:37 +0200

    naxor

--- plugins/carbon.js
+++ plugins/carbon.js
@@ -1,11 +1,12 @@
-const {Module} = require('../lib/plugins');
+const { Module } = require('../lib/plugins');
 
 Module({
   command: "carbon",
   package: "tools"
 })(async (message, match) => {
   if (!match) return message.send("_Please provide code_");
-  let code = match; 
+  let code = match;
   let url = `https://api.naxordeve.qzz.io/tools/carbon?code=${code}`;
-  await message.send({ image: url, caption: "*Carbon*" });
+  await message.send({image: { url },caption: "*Carbon*"
+  });
 });
