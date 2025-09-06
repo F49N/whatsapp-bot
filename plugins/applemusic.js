83f2f56d2696db3a05db3d6bbfdfda4a9b66c773
naxor <naxordeve@gmail.com>
2025-09-06 16:50:44 +0200

    naxor

--- plugins/applemusic.js
+++ plugins/applemusic.js
@@ -1,13 +1,17 @@
-var { Module } = require('../lib/plugins')
+const { Module } = require('../lib/plugins')
 var axios = require('axios')
 
-Module({ command: "applemusic",
-       package: "downloader"
+Module({ command: "applemusic", package: "downloader" 
 })(async (message, match) => {
   if (!match) return message.send("_Give an Apple Music url_")
   let res = await axios.get(`https://api.naxordeve.qzz.io/download/applemusic?url=${match}`)
   if (!res.data.success || !res.data.links) return message.send("err")
   let mp3 = res.data.links.find(x => x.name.includes("Mp3"))
   if (!mp3) return message.send("_Not found_")
-  await message.conn.sendMessage(message.from,{ document: { url: mp3.url }, mimetype: "audio/mpeg", fileName: mp3.name + ".mp3" },{ quoted: message })
+  let token = new URL(mp3.url).searchParams.get("token")
+  let payload = token.split(".")[0]
+  let json = Buffer.from(payload, "base64").toString()
+  let title = JSON.parse(json).name
+  await message.send({ document: { url: mp3.url },mimetype: "audio/mpeg",fileName: `${title}.mp3`
+  })
 })
